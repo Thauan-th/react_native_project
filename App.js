54a93087264a1be454046b1c648887c410388fe4
Thauan <83513806+Thauan-th@users.noreply.github.com>
2022-05-31 14:39:28 -0300

    usando api para uso de camera

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import CameraComponente from './src/components/camera'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Codando mobile
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+			<CameraComponente/> );
 }
 
 const styles = StyleSheet.create({
