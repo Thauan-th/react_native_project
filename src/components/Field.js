f0570132a17045688123cea927a8fb291295d2ba
Thauan <83513806+Thauan-th@users.noreply.github.com>
2022-07-26 12:01:04 -0300

    update: componente campo

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 import {Field} from './styles'
 
-export default () => {
+export default ({
+  mined,
+  opened,
+  nearMines
+}) => {
+  
   const styled_class=['default']
 
+  if (opened) styled_class.push('opened')
   if (styled_class.length==1) styled_class.push('regular')
 
   return (
